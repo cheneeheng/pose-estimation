8bdeab98d5ce65fd9ce7e598ee1cd563530917db
cheneeheng <eeheng.chen@gmail.com>
2023-02-27 15:28:10 +0100

    added docker files for cpu

---
+++ docker/openpose/build_openpose_u20_cpu.sh
@@ -0,0 +1,30 @@
+#! /bin/sh
+
+# This script builds openpose image.
+# 2 ARGS IN ORDER: {USERNAME}, {PID}
+
+BASE_IMAGE="ubuntu:20.04"
+IMAGE_NAME="openpose:ubuntu20.04"
+DOCKER_FILE="dockerfiles/Dockerfile.U20OpenposeCPU"
+
+# BUILD OPENPOSE IMAGE ---------------------------------------------------------
+echo "Building image : ${IMAGE_NAME}"
+DOCKER_BUILDKIT=1 docker build \
+    --file ${DOCKER_FILE} \
+    --build-arg BASE_IMAGE=${BASE_IMAGE} \
+    --tag "${IMAGE_NAME}" \
+    .
+echo "Built image : ${IMAGE_NAME}\n"
+
+# ADD USER ---------------------------------------------------------------------
+if [ $# -gt 0 ]; then
+    mkdir tmp
+    P=$(pwd)
+    cp -rf ../_user tmp
+    cd tmp/_user
+    echo "adding user..."
+    sh build.sh $1 $2 ${IMAGE_NAME}
+    echo "added user...\n"
+    cd $P
+    rm -rf tmp
+fi
