d79d031de505106e60b3e00de23cabe9a68b17f0
chen <eeheng.chen@tum.de>
2023-04-21 11:05:37 +0200

    updated action recognition demo

--- demo/action_recognition.py
+++ demo/action_recognition.py
@@ -4,55 +4,12 @@ import json
 import numpy as np
 import os
 import time
-import yaml
-import random
 
-from functools import partial
-from typing import Tuple
+from infer.inference import ActionRecognition
 
-import torch
-from torch.nn import functional as F
-
-from data_gen.preprocess import pre_normalization
-from feeders.loader import NTUDataLoaders
-from infer.data_preprocess import DataPreprocessorV2
 from utils.parser import get_parser as get_default_parser
-from utils.utils import import_class
-
-
-def filter_logits(logits: list) -> Tuple[list, list]:
-    # {
-    #     "8": "sitting down",
-    #     "9": "standing up (from sitting position)",
-    #     "10": "clapping",
-    #     "23": "hand waving",
-    #     "26": "hopping (one foot jumping)",
-    #     "27": "jump up",
-    #     "35": "nod head/bow",
-    #     "36": "shake head",
-    #     "43": "falling",
-    #     "56": "giving something to other person",
-    #     "58": "handshaking",
-    #     "59": "walking towards each other",
-    #     "60": "walking apart from each other"
-    # }
-    ids = [7, 8, 9, 22, 25, 27, 34, 35, 42, 55, 57, 58, 59]
-    sort_idx = np.argsort(-np.array(logits)).tolist()
-    sort_idx = [i for i in sort_idx if i in ids]
-    new_logits = [logits[i] for i in sort_idx]
-    return sort_idx, new_logits
-
-
-def init_seed(seed=1):
-    os.environ['PYTHONHASHSEED'] = str(seed)
-    torch.cuda.manual_seed_all(seed)
-    torch.manual_seed(seed)
-    np.random.seed(seed)
-    random.seed(seed)
-    # torch.backends.cudnn.enabled = False
-    torch.backends.cudnn.deterministic = True
-    torch.backends.cudnn.benchmark = False
-    # torch.use_deterministic_algorithms(True)
+from utils.parser import load_parser_args_from_config
+from utils.utils import init_seed
 
 
 if __name__ == '__main__':
@@ -60,75 +17,40 @@ if __name__ == '__main__':
     init_seed(1)
 
     # Parse args ---------------------------------------------------------------
+    # data_path = "/data/07_AAGCN/data_tmp/S003C001P018R001A009_15j"
+    # data_path = "/data/07_AAGCN/data_tmp/S003C001P018R001A027_15j"
+    # data_path = "/data/07_AAGCN/data_tmp/S003C001P018R001A031_15j"
+    # data_path = "/data/07_AAGCN/data_tmp/S003C001P002R001A031_15j"
+    data_path = "/data/07_AAGCN/data_tmp/S003C001P018R001A043_15j"
+    # data_path = "/data/07_AAGCN/data_tmp/S003C001P018R001A056_15j"
+    # label_mapping_file = "/data/07_AAGCN/model/ntu_15j/index_to_name.json"
+    label_mapping_file = "/data/07_AAGCN/model/ntu_15j_9l/index_to_name.json"
+    # weights = "/data/07_AAGCN/data/openpose_b25_j15_ntu_result/xview/aagcn_preprocess_sgn_model/230414100001/weight/SGN-116-68208.pt"  # noqa
+    weights = "/data/07_AAGCN/data/openpose_b25_j15_9l_ntu_result/xview/aagcn_preprocess_sgn_model/230414100001/weight/SGN-110-10670.pt"  # noqa
+    # weights = "/data/07_AAGCN/data/openpose_b25_j15_ntu_result/xview/aagcn_joint/230414100001/weight/Model-50-29400.pt"  # noqa
+    # weights = "/data/07_AAGCN/data/openpose_b25_j15_9l_ntu_result/xview/aagcn_joint/230414100001/weight/Model-50-4850.pt"  # noqa
+    # config = "/data/07_AAGCN/data/openpose_b25_j15_ntu_result/xview/aagcn_preprocess_sgn_model/230414100001/config.yaml"  # noqa
+    config = "/data/07_AAGCN/data/openpose_b25_j15_9l_ntu_result/xview/aagcn_preprocess_sgn_model/230414100001/config.yaml"  # noqa
+    # config = "/data/07_AAGCN/data/openpose_b25_j15_ntu_result/xview/aagcn_joint/230414100001/config.yaml"  # noqa
+    # config = "/data/07_AAGCN/data/openpose_b25_j15_9l_ntu_result/xview/aagcn_joint/230414100001/config.yaml"  # noqa
     parser = get_default_parser()
-    parser.add_argument('--max-frame', type=int, default=300)
-    parser.add_argument('--max-num-skeleton-true', type=int, default=2)  # noqa
-    parser.add_argument('--max-num-skeleton', type=int, default=4)  # noqa
-    parser.add_argument('--num-joint', type=int, default=15)
-
-    parser.add_argument('--gpu', type=bool, default=True)
-    parser.add_argument('--timing', type=bool, default=False)
-    parser.add_argument('--interval', type=int, default=0)
-    parser.add_argument('--moving-avg', type=int, default=1)
-
-    parser.add_argument('--aagcn-normalize', type=bool, default=True)
-    parser.add_argument('--sgn-preprocess', type=bool, default=True)
-    parser.add_argument('--multi-test', type=int, default=5)
-
-    parser.add_argument(
-        '--data-path',
-        type=str,
-        # default='data/data_tmp/S003C001P018R001A009_15j'
-        # default='data/data_tmp/S003C001P018R001A027_15j'
-        # default='data/data_tmp/S003C001P018R001A031_15j'
-        # default='data/data_tmp/S003C001P002R001A031_15j'
-        default='/data/07_AAGCN/data_tmp/S003C001P018R001A043_15j'
-        # default='data/data_tmp/S003C001P018R001A056_15j'
-    )
-    parser.add_argument(
-        '--label-mapping-file',
-        type=str,
-        # default='data/model/ntu_15j/index_to_name.json'
-        default='/data/07_AAGCN/model/ntu_15j_9l/index_to_name.json'
-    )
-    parser.add_argument(
-        '--weight-file',
-        type=str,
-        # default='data/data/openpose_b25_j15_ntu_result/xview/aagcn_preprocess_sgn_model/230414100001/weight/SGN-116-68208.pt'  # noqa
-        default='/data/07_AAGCN/data/openpose_b25_j15_9l_ntu_result/xview/aagcn_preprocess_sgn_model/230414100001/weight/SGN-110-10670.pt'  # noqa
-        # default='data/data/openpose_b25_j15_ntu_result/xview/aagcn_joint/230414100001/weight/Model-50-29400.pt'  # noqa
-        # default='data/data/openpose_b25_j15_9l_ntu_result/xview/aagcn_joint/230414100001/weight/Model-50-4850.pt'  # noqa
-    )
-    parser.add_argument(
-        '--config-file',
-        type=str,
-        # default='data/data/openpose_b25_j15_ntu_result/xview/aagcn_preprocess_sgn_model/230414100001/config.yaml'  # noqa
-        default='/data/07_AAGCN/data/openpose_b25_j15_9l_ntu_result/xview/aagcn_preprocess_sgn_model/230414100001/config.yaml'  # noqa
-        # default='data/data/openpose_b25_j15_ntu_result/xview/aagcn_joint/230414100001/config.yaml'  # noqa
-        # default='data/data/openpose_b25_j15_9l_ntu_result/xview/aagcn_joint/230414100001/config.yaml'  # noqa
-    )
-    parser.add_argument(
-        '--out-folder',
-        type=str,
-        # default='data/data_tmp/inference_predictions_aagcn_preprocess_sgn_model'  # noqa
-        # default='data/data_tmp/inference_predictions_aagcn_joint'
-        # default='data/data_tmp/inference_predictions_aagcn_preprocess_sgn_model_9l'  # noqa
-        # default='data/data_tmp/inference_predictions_aagcn_joint_9l'
-        default='/data/07_AAGCN/data_tmp/delme'
-    )
-
-    # PARSE CONFIGS FROM YAML
-    [p, _] = parser.parse_known_args()
-    with open(p.config_file, 'r') as f:
-        default_arg = yaml.safe_load(f)
-    key = vars(p).keys()
-    for k in default_arg.keys():
-        if k not in key:
-            print(f'WRONG ARG: {k}')
-            assert (k in key)
-    parser.set_defaults(**default_arg)
-
-    [args, _] = parser.parse_known_args()
+    parser.set_defaults(**{'config': config})
+    args = load_parser_args_from_config(parser)
+    args.max_frame = 300
+    args.max_num_skeleton_true = 2
+    args.max_num_skeleton = 4
+    args.num_joint = 15
+    args.gpu = True
+    args.timing = False
+    args.interval = 0
+    args.moving_avg = 1
+    args.aagcn_normalize = True
+    args.sgn_preprocess = True
+    args.multi_test = 5
+    args.out_folder = "/data/07_AAGCN/data_tmp/delme"
+    args.data_path = data_path
+    args.weights = weights
+    args.label_mapping_file = label_mapping_file
 
     # prepare file and folders -------------------------------------------------
     # action id mapper
@@ -140,34 +62,8 @@ if __name__ == '__main__':
     output_dir = args.out_folder
     os.makedirs(output_dir, exist_ok=True)
 
-    # Data processor -----------------------------------------------------------
-    fn = NTUDataLoaders(dataset='NTU60',
-                        seg=20,
-                        multi_test=args.multi_test).to_fix_length
-    sgn_preprocess_fn = partial(fn,
-                                labels=None,
-                                sampling_frequency=args.multi_test)
-    aagcn_normalize_fn = partial(pre_normalization,
-                                 zaxis=[8, 1],
-                                 xaxis=[2, 5],
-                                 verbose=False,
-                                 tqdm=False)
-    DataProc = DataPreprocessorV2(num_joint=args.num_joint,
-                                  max_seq_length=args.max_frame,
-                                  max_person=args.max_num_skeleton,
-                                  moving_avg=args.moving_avg,
-                                  aagcn_normalize_fn=aagcn_normalize_fn,
-                                  sgn_preprocess_fn=sgn_preprocess_fn)
-
-    # Prepare model ------------------------------------------------------------
-    Model = import_class(args.model)(**args.model_args)
-    Model.eval()
-    Model.load_state_dict(torch.load(args.weight_file))
-    if args.gpu:
-        Model = Model.cuda(0)
-    if int(torch.__version__.split('.')[0]) == 2:
-        Model = torch.compile(Model)
-    print("Model loaded...")
+    # Setup action recognition -------------------------------------------------
+    AR = ActionRecognition(args)
 
     # MAIN LOOP ----------------------------------------------------------------
     start = time.time()
@@ -218,43 +114,24 @@ if __name__ == '__main__':
                                                body_joint[j+2]]
                     else:
                         pass
-            # data  # M, T, V, C
-            DataProc.append_data(data[:args.max_num_skeleton_true, :, :, :])
+
+            # data  # M, 1, V, C
+            AR.append_data(data)
 
             # 2. Batch frames to fixed length. ---------------------------------
             # 3. Normalization. ------------------------------------------------
-            input_data = DataProc.select_skeletons_and_normalize_data(
-                args.max_num_skeleton_true,
-                aagcn_normalize=args.aagcn_normalize,
-                sgn_preprocess=args.sgn_preprocess
-            )
-
             # 4. Inference. ----------------------------------------------------
-            with torch.no_grad():
-                torch_input = torch.from_numpy(input_data)
-                if args.gpu:
-                    torch_input = torch_input.cuda(0)
-                output, _ = Model(torch_input)
-                if 'sgn' in args.model:
-                    output = output.view((-1, args.multi_test, output.size(1)))
-                    output = output.mean(1)
-                output = F.softmax(output, 1)
-                _, predict_label = torch.max(output, 1)
-                if args.gpu:
-                    output = output.data.cpu()
-                    predict_label = predict_label.data.cpu()
-
-            logits, preds = output.tolist(), predict_label.item()
+            logits, preds = AR.predict()
 
             if args.timing and idx > 10:
                 end_time = time.time() - start_time
                 start_time = time.time()
                 print(f"{skel_file} , {end_time:.4f}s , "
-                      f"{preds+1} , {logits[0][preds]:.2f} , "
+                      f"{preds+1} , {logits[preds]:.2f} , "
                       f"{MAPPING[preds+1]}")
             else:
                 print(f"{skel_file} , "
-                      f"{preds+1} , {logits[0][preds]:.2f} , "
+                      f"{preds+1} , {logits[preds]:.2f} , "
                       f"{MAPPING[preds+1]}")
 
         break
--- demo/demo.py
+++ demo/demo.py
@@ -1,6 +1,12 @@
+import json
+import numpy as np
 import os
 import time
 import sys
+from functools import partial
+
+import torch
+from torch.nn import functional as F
 
 from rs_py.rs_run_devices import get_rs_parser
 from rs_py.rs_run_devices import printout
@@ -10,6 +16,11 @@ from openpose.native.python.inference_rs import get_parser as get_op_parser
 from openpose.native.python.inference_rs import OpenPosePoseExtractor
 from openpose.native.python.inference_rs import Tracker
 from openpose.native.python.inference_rs import ExtractSkeletonAndTrack
+
+from infer.inference import ActionRecognition
+from utils.parser import get_parser as get_ar_parser
+from utils.parser import load_parser_args_from_config
+from utils.utils import init_seed
 
 
 def get_rs_args():
@@ -36,7 +47,7 @@ def get_op_args():
     args.op_model_pose = "BODY_25"
     args.op_net_resolution = "-1x368"
     args.op_skel_thres = 0.2
-    args.op_max_true_body = 8
+    args.op_max_true_body = 4
     # args.op_heatmaps_add_parts = True
     # args.op_heatmaps_add_bkg = True
     # args.op_heatmaps_add_PAFs = True
@@ -78,6 +89,88 @@ def get_op_args():
     return args
 
 
+def get_ar_args():
+    init_seed(1)
+    # label_mapping_file = "/data/07_AAGCN/model/ntu_15j/index_to_name.json"
+    label_mapping_file = "/data/07_AAGCN/model/ntu_15j_9l/index_to_name.json"
+    # weights = "/data/07_AAGCN/data/openpose_b25_j15_ntu_result/xview/aagcn_preprocess_sgn_model/230414100001/weight/SGN-116-68208.pt"  # noqa
+    weights = "/data/07_AAGCN/data/openpose_b25_j15_9l_ntu_result/xview/aagcn_preprocess_sgn_model/230414100001/weight/SGN-110-10670.pt"  # noqa
+    # weights = "/data/07_AAGCN/data/openpose_b25_j15_ntu_result/xview/aagcn_joint/230414100001/weight/Model-50-29400.pt"  # noqa
+    # weights = "/data/07_AAGCN/data/openpose_b25_j15_9l_ntu_result/xview/aagcn_joint/230414100001/weight/Model-50-4850.pt"  # noqa
+    # config = "/data/07_AAGCN/data/openpose_b25_j15_ntu_result/xview/aagcn_preprocess_sgn_model/230414100001/config.yaml"  # noqa
+    config = "/data/07_AAGCN/data/openpose_b25_j15_9l_ntu_result/xview/aagcn_preprocess_sgn_model/230414100001/config.yaml"  # noqa
+    # config = "/data/07_AAGCN/data/openpose_b25_j15_ntu_result/xview/aagcn_joint/230414100001/config.yaml"  # noqa
+    # config = "/data/07_AAGCN/data/openpose_b25_j15_9l_ntu_result/xview/aagcn_joint/230414100001/config.yaml"  # noqa
+    parser = get_ar_parser()
+    parser.set_defaults(**{'config': config})
+    args = load_parser_args_from_config(parser)
+    args.weights = weights
+    args.max_frame = 300
+    args.max_num_skeleton_true = 2
+    args.max_num_skeleton = 4
+    args.num_joint = 15
+    args.gpu = True
+    args.timing = False
+    args.interval = 0
+    args.moving_avg = 1
+    args.aagcn_normalize = True
+    args.sgn_preprocess = True
+    args.multi_test = 5
+    # args.data_path = data_path
+    args.label_mapping_file = label_mapping_file
+    # args.out_folder = "/data/07_AAGCN/data_tmp/delme"
+    print("========================================")
+    print(">>>>> ar_args <<<<<")
+    print("========================================")
+    for k, v in vars(args).items():
+        print(f"{k} : {v}")
+    print("========================================")
+    return args
+
+
+class SkeletonStorage():
+    def __init__(self) -> None:
+        self.ids = []
+        self.skeletons = []
+        self.counter = []
+        self.valid = []
+
+    def add(self, track_id, keypoints):
+        if track_id in self.ids:
+            sid = self.ids.index(track_id)
+            self.skeletons[sid].append(keypoints)
+            self.valid.append(True)
+        else:
+            self.ids.append(track_id)
+            self.skeletons.append([keypoints])
+            self.counter.append(0)
+            self.valid.append(True)
+
+    def get_last_skel(self):
+        skels = []
+        for skeleton in self.skeletons:
+            skels.append(skeleton[-1])
+        return np.expand_dims(np.stack(skels, 0), 1)  # M, 1, V, C
+
+    def delete(self, idx):
+        self.ids.pop(idx)
+        self.skeletons.pop(idx)
+        self.counter.pop(idx)
+        self.valid.pop(idx)
+
+    def check_valid_and_delete(self):
+        num_data = len(self.ids)
+        del_idx = []
+        for i in range(num_data):
+            if not self.valid[i]:
+                self.counter[i] += 1
+            if self.counter[i] > 30:
+                del_idx.append(self.ids[i])
+            self.valid[i] = False
+        for i in del_idx:
+            self.delete(i)
+
+
 if __name__ == "__main__":
 
     # Delay = predict and no update in tracker
@@ -92,12 +185,12 @@ if __name__ == "__main__":
     runtime = {'PE': [], 'TK': []}
 
     # For skeleton storage
-    skeletons = {}  # {trackid: (v,c)}
-    skeletons_age = {}  # {trackid: counter }
+    skelstorage = SkeletonStorage()
 
     # 1. args ------------------------------------------------------------------
     rs_args = get_rs_args()
     op_args = get_op_args()
+    ar_args = get_ar_args()
 
     # 2. setup realsense -------------------------------------------------------
     rsw = RealsenseWrapper(rs_args, rs_args.rs_dev)
@@ -120,6 +213,14 @@ if __name__ == "__main__":
     EST = ExtractSkeletonAndTrack(
         op_args, PoseExtractor, PoseTracker, enable_timer)
     EST.start()
+
+    # 4. setup aagcn -----------------------------------------------------------
+    with open(ar_args.label_mapping_file, 'r') as f:
+        MAPPING = {int(i): j for i, j in json.load(f).items()}
+
+    AR = ActionRecognition(ar_args)
+
+    ar_start_time = time.time()
 
     # MAIN LOOP ----------------------------------------------------------------
     try:
@@ -162,32 +263,10 @@ if __name__ == "__main__":
                 (filered_skel, prep_time, infer_time, track_time, _
                  ) = EST.queue_output()
 
-                if EST.PE.pyop.pose_keypoints is not None:
-                    # skeletons[].append()
-                    print(EST.PE.pyop.pose_keypoints.shape)  # m,v,c
-                    # if tracks is not None:
-                    #     for track in tracks:
-                    #         try:
-                    #             # deepsort / ocsort
-                    #             bb = track.to_tlbr()
-                    #         except AttributeError:
-                    #             # bytetrack
-                    #             bb = track.tlbr
-                    #         l, t, r, b = bb
-                    #         tl = (np.floor(l).astype(int),
-                    #               np.floor(t).astype(int))
-                    #         br = (np.ceil(r).astype(int),
-                    #               np.ceil(b).astype(int))
-                    #         image = cv2.rectangle(image, tl, br,
-                    #                               get_color(track.track_id), 1)
-                    #         cv2.putText(image,
-                    #                     f"ID : {track.track_id}",
-                    #                     tl,
-                    #                     cv2.FONT_HERSHEY_PLAIN,
-                    #                     2,
-                    #                     get_color(track.track_id),
-                    #                     2,
-                    #                     cv2.LINE_AA)
+                for track in EST.TK.tracks:
+                    skelstorage.add(track.track_id,
+                                    EST.PE.pyop.pose_keypoints[track.det_id])
+                skelstorage.check_valid_and_delete()
 
                 status = EST.PE.display(win_name="000",
                                         speed=display_speed,
@@ -198,7 +277,25 @@ if __name__ == "__main__":
                 if not status[0]:
                     break
 
-            # 7. printout ------------------------------------------------------
+            # 7. action recognition --------------------------------------------
+            if time.time() - ar_start_time > int(ar_args.interval):
+                if enable_timer:
+                    start_time = time.time()
+
+                if len(skelstorage.skeletons) > 0:
+                    # data  # M, 1, V, C
+                    AR.append_data(skelstorage.get_last_skel())
+
+                    # logits = [#labels], prediction = label from 0 - #labels-1
+                    logits, prediction = AR.predict()
+                    print(MAPPING[prediction+1])
+
+                if enable_timer:
+                    recog_time = -1
+                else:
+                    recog_time = time.time() - start_time
+
+            # 8. printout ------------------------------------------------------
             if c % rs_args.rs_fps == 0:
                 # printout(
                 #     f"Step {c:12d} :: "
@@ -216,6 +313,7 @@ if __name__ == "__main__":
                     f"Prep time : {prep_time:.3f} | "
                     f"Pose time : {infer_time:.3f} | "
                     f"Track time : {track_time:.3f} | "
+                    f"Recog time : {recog_time:.3f} | "
                     f"FPS PE : {sum(runtime['PE'])/len(runtime['PE']):.3f} | "
                     f"FPS TK : {sum(runtime['TK'])/len(runtime['TK']):.3f}",
                     'i'
